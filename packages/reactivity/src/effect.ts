547d080c08875fdb7558db288f48d3e35f28361b
xiaoyao <xiaoyao9524@163.com>
2023-04-07 11:03:31 +0800

    初步实现reactive、effect收集依赖并触发依赖

--- packages/reactivity/src/effect.ts
+++ packages/reactivity/src/effect.ts
@@ -1,3 +1,34 @@
+type KeyToDepMap = Map<any, ReactiveEffect>;
+
+/**
+ * targetMap:
+ *    key: 响应性对象
+ *    value: Map对象
+ *        key: 响应对象的指定属性
+ *        value: 指定对象属性的执行函数
+ */
+const targetMap = new WeakMap<object, KeyToDepMap>();
+
+/**
+ * 当前激活的依赖
+ */
+let activeEffect: ReactiveEffect | undefined;
+
+export function effect<T = any>(fn: () => T) {
+  const _effect = new ReactiveEffect<T>(fn);
+
+  _effect.run();
+}
+
+export class ReactiveEffect<T = any> {
+  constructor(public fn: () => T) {}
+
+  run() {
+    activeEffect = this;
+    return this.fn();
+  }
+}
+
 /**
  * 收集依赖
  * @param target
@@ -5,6 +36,20 @@
  */
 export function track(target: object, key: unknown) {
   console.log("收集依赖");
+
+  if (!activeEffect) {
+    return;
+  }
+
+  let depsMap = targetMap.get(target);
+
+  if (!depsMap) {
+    targetMap.set(target, (depsMap = new Map()));
+  }
+
+  depsMap.set(key, activeEffect);
+
+  console.log("targetMap: ", targetMap);
 }
 
 /**
@@ -15,4 +60,17 @@ export function track(target: object, key: unknown) {
  */
 export function trigger(target: object, key: unknown, newValue: unknown) {
   console.log("触发依赖");
+
+  const depsMap = targetMap.get(target);
+  if (!depsMap) {
+    return;
+  }
+
+  const effect = depsMap.get(key) as ReactiveEffect;
+
+  if (!effect) {
+    return;
+  }
+
+  effect.fn();
 }
